6a7c7489424bf3db3eb38f828a2d5e40b9a7399b
basantsaed2 <basantsaed718@gmail.com>
2026-05-05 08:05:10 +0300

    edit

--- dist/middleware/index.js
+++ dist/middleware/index.js
@@ -3,6 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.validation = void 0;
 const validation = (schema) => {
     return (req, res, next) => {
+        console.log(Object.keys(schema));
+        for (const key of Object.keys(schema)) {
+            if (!schema[key]) {
+                continue;
+            }
+            const values = schema[key].safeParse(req[key]);
+            console.log(values);
+        }
     };
 };
 exports.validation = validation;
